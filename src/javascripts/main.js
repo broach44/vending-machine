ec1a67c0a1752053f3a501c33f33e8bb4af24d15
Crystal Broach <crysland@gmail.com>
2019-10-29 21:41:10 -0500

    authorization functions

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,14 +1,19 @@
 import firebase from 'firebase';
 
+import auth from './components/Auth/auth';
+import authData from './helpers/data/authData';
+import myNavbar from './components/MyNavbar/myNavbar';
+
 import apiKeys from './helpers/apiKeys.json';
 
 import 'bootstrap';
 import '../styles/main.scss';
 
 const init = () => {
-  console.log(apiKeys.firebaseKeys);
   firebase.initializeApp(apiKeys.firebaseKeys);
-  console.log(firebase.apps);
+  authData.checkLoginStatus();
+  myNavbar.logoutEvent();
+  auth.loginButton();
 };
 
 init();

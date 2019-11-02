abc4751f149a38a0faf4ee51182036d88e484c80
Crystal Broach <crysland@gmail.com>
2019-11-02 14:16:00 -0500

    printing snacks to the page

--- src/javascripts/components/Machine/machine.js
+++ src/javascripts/components/Machine/machine.js
@@ -1,10 +1,25 @@
 import smash from '../../helpers/data/smash';
+import utilities from '../../helpers/utilities';
+import snackComponent from '../Snacks/snacks';
 
 import './machine.scss';
 
 const buildTheMachine = () => {
   smash.getCompleteMachine()
-    .then((singleMachine) => console.log('1 machine', singleMachine))
+    .then((positions) => {
+      let domString = '<h2>VENDING MACHINE</h2>';
+      domString += '<div id="snack-section class="d-flex flex-wrap">';
+      positions.forEach((position) => {
+        domString += snackComponent.makeASnack(position);
+      });
+      domString += '</div>';
+      // snacks component should return a bootstrap card
+      // positions.forEach((x) => {
+      //   console.log('positions', x.position);
+      // });
+      // domString += positions[0].snack.name;
+      utilities.printToDom('stock', domString);
+    })
     .catch((error) => console.error(error));
 };
 
